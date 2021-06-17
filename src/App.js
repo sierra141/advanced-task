5df8e0d146f61932662289198df36315945f23ee
sierra141 <marko-st4@hotmail.com>
2021-06-17 23:10:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button';
+import ColorsList from './components/ColorsList';
+import { useState } from 'react';
+import axios from 'axios';
+import { validateHTMLColorHex } from "validate-color";
 
-function App() {
+
+
+const App = () => {
+
+  const [colors, setColors] = useState([]);
+  const [text, setText] = useState('Get Color');
+  const [buttonColor, setButtonColor] = useState();
+
+
+
+  const loadData = async () => {
+    const data = await axios.get(`https://www.colr.org/json/color/random?timestamp=${Date.now()}`, {cache: "no-cache"});
+    if (data.data.new_color !== ""){
+      addColor(data);
+      setButtonColor(data.data.new_color);
+    }
+   
+  }
+
+  const addColor = (data) => {
+    const id = data.data.colors[0].id;
+    const id_s = id.toString();
+    const hex = data.data.new_color;
+    const newColor = { id_s, hex };
+    if (!colors.some(color => color.hex === newColor.hex) || newColor.hex !== ""){
+        setColors([...colors, newColor]);
+        setText("Get Color");
+        setButtonColor(data.data.new_color);
+    }
+    
+
+  }
+  const handleKeypress = e => {
+    if (e.key === 'Enter') {      
+      if (validateHTMLColorHex(text)){
+        const id = Math.floor(Math.random() * 1000) + 1;
+        const id_s = id.toString();
+        const hex = text.substring(1);
+        const manualColor = { id_s, hex };
+        if (!colors.some(color => color.hex === manualColor.hex)){
+          setColors([...colors, manualColor]);
+          setButtonColor(manualColor.hex);
+          setText("Get Color");
+        }
+        else {
+          alert("Color already exists!");
+        };
+      }
+      else {
+        alert("Not a valid hex color!");
+      };
+    }
+    
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+          <h3 className="label">Advanced Task</h3>
+          <Button onClick={loadData} color={"#" + buttonColor} text={text}></Button>
+          <ColorsList colors={colors} buttonColor={buttonColor}/>
+          <input onChange={event => setText(event.target.value)} placeholder="Enter CSS hex color code, example: #FFA500" type="text" onKeyPress={handleKeypress}/>
       </header>
     </div>
   );
---
+++ src/components/ColorsList.js
@@ -0,0 +1,37 @@
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
+
+
+const ColorsList = ({ colors, buttonColor }) => {
+
+    return (
+        <DragDropContext onDragEnd={(param) =>{
+            const srcIndex = param.source.index;
+            const destIndex = param.destination?.index;
+            if(destIndex){
+            colors.splice(destIndex, 0, colors.splice(srcIndex, 1)[0]);
+            }
+        }}>
+            <Droppable droppableId="colorList">
+                {(provided) => (
+                <ul {...provided.droppableProps} ref={provided.innerRef} style={{ listStyleType: "none" }}>
+            {colors.map((color, index) => (
+                <Draggable key={color.id_s} draggableId={color.id_s} index={index}>
+                    {(provided) => (
+
+                        <li id={color.id_s} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                            <h4 style={(color.hex !== buttonColor) ? {color: "#" + color.hex, fontWeight: "200"} : {color: "#" + color.hex ,fontWeight: "1000"}}>#{color.hex}</h4>
+                        </li>
+                        
+                    )}
+                </Draggable>
+            ))}
+            {provided.placeholder}
+        </ul>
+        )}
+        </Droppable>
+        </DragDropContext>
+    )
+}
+
+
+export default ColorsList
